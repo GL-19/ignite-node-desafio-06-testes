5ab9a32d0a59c39d01afe52e35753ce886fd8861
GL-19 <loiolagl19@gmail.com>
2022-02-10 22:59:57 -0300

    Criado testes unitários do CreateUserUseCase

--- src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++
@@ -1,6 +0,0 @@
-describe("Authenticate User", () => {
-  it("should test", () => {
-    const a = 2;
-    expect(a).toBe(2)
-  })
-})
---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,48 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserError } from "./CreateUserError";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+import { compare } from "bcryptjs";
+
+let createUserUseCase: CreateUserUseCase;
+let usersRepository: InMemoryUsersRepository;
+
+describe("Create User", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(usersRepository);
+  });
+
+  it("should be able to create a new user", async () => {
+    const password = "12345";
+
+    const user = await createUserUseCase.execute({
+      name: "Glauber Loiola",
+      email: "glauber@email.com",
+      password,
+    });
+
+    const isEqual = await compare(password, user.password);
+
+    expect(user).toHaveProperty("id");
+    expect(user.name).toBe("Glauber Loiola");
+    expect(user.email).toBe("glauber@email.com");
+    expect(isEqual).toBe(true);
+  });
+
+  it("should not be able to create a new user if email already exists", async () => {
+    expect(async () => {
+      await createUserUseCase.execute({
+        name: "Glauber Loiola",
+        email: "glauber@email.com",
+        password: "12345",
+      });
+
+      await createUserUseCase.execute({
+        name: "Glauber Loiola",
+        email: "glauber@email.com",
+        password: "12345",
+      });
+    }).rejects.toBeInstanceOf(CreateUserError);
+  });
+});
