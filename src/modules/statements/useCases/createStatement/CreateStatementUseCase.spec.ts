860d21581dbbdb34b8af01cf4a0807a7b69a32f7
GL-19 <loiolagl19@gmail.com>
2022-02-14 22:26:42 -0300

    Criados testes unitários do CreateStatementUseCase

---
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -0,0 +1,114 @@
+import { OperationType } from "@modules/statements/entities/Statement";
+import { InMemoryStatementsRepository } from "@modules/statements/repositories/in-memory/InMemoryStatementsRepository";
+import { InMemoryUsersRepository } from "@modules/users/repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "@modules/users/useCases/createUser/CreateUserUseCase";
+import { CreateStatementError } from "./CreateStatementError";
+import { CreateStatementUseCase } from "./CreateStatementUseCase";
+import { ICreateStatementDTO } from "./ICreateStatementDTO";
+
+let usersRepository: InMemoryUsersRepository;
+let statementsRepository: InMemoryStatementsRepository;
+let createUserUseCase: CreateUserUseCase;
+let createStatementUseCase: CreateStatementUseCase;
+
+describe("Create Statemente", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    statementsRepository = new InMemoryStatementsRepository();
+    createUserUseCase = new CreateUserUseCase(usersRepository);
+    createStatementUseCase = new CreateStatementUseCase(
+      usersRepository,
+      statementsRepository
+    );
+  });
+
+  it("should be able to create a deposit statement", async () => {
+    const user = await createUserUseCase.execute({
+      name: "Glauber Loiola",
+      email: "glauber@email.com",
+      password: "12345",
+    });
+
+    const statement = await createStatementUseCase.execute({
+      user_id: user.id,
+      type: OperationType.DEPOSIT,
+      amount: 500,
+      description: "test deposit",
+    });
+
+    expect(statement).toHaveProperty("id");
+    expect(statement.type).toBe(OperationType.DEPOSIT);
+    expect(statement.user_id).toBe(user.id);
+    expect(statement.amount).toBe(500);
+    expect(statement.description).toBe("test deposit");
+  });
+
+  it("should not be able to create a statement if user does not exist", async () => {
+    expect(async () => {
+      const statement: ICreateStatementDTO = {
+        user_id: "test",
+        type: OperationType.DEPOSIT,
+        amount: 400,
+        description: "test deposit",
+      };
+
+      await createStatementUseCase.execute(statement);
+    }).rejects.toBeInstanceOf(CreateStatementError.UserNotFound);
+
+    expect(async () => {
+      const statement: ICreateStatementDTO = {
+        user_id: "test",
+        type: OperationType.WITHDRAW,
+        amount: 400,
+        description: "test withdraw",
+      };
+
+      await createStatementUseCase.execute(statement);
+    }).rejects.toBeInstanceOf(CreateStatementError.UserNotFound);
+  });
+
+  it("should be able to create a withdraw statement", async () => {
+    const user = await createUserUseCase.execute({
+      name: "Glauber Loiola",
+      email: "glauber@email.com",
+      password: "12345",
+    });
+
+    await createStatementUseCase.execute({
+      user_id: user.id,
+      type: OperationType.DEPOSIT,
+      amount: 500,
+      description: "test deposit",
+    });
+
+    const statement = await createStatementUseCase.execute({
+      user_id: user.id,
+      type: OperationType.WITHDRAW,
+      amount: 300,
+      description: "test withdraw",
+    });
+
+    expect(statement).toHaveProperty("id");
+    expect(statement.type).toBe(OperationType.WITHDRAW);
+    expect(statement.user_id).toBe(user.id);
+    expect(statement.amount).toBe(300);
+    expect(statement.description).toBe("test withdraw");
+  });
+
+  it("should not be able to create a withdraw if there is not enough funds", async () => {
+    expect(async () => {
+      const user = await createUserUseCase.execute({
+        name: "Glauber Loiola",
+        email: "glauber@email.com",
+        password: "12345",
+      });
+
+      const statement = await createStatementUseCase.execute({
+        user_id: user.id,
+        type: OperationType.WITHDRAW,
+        amount: 300,
+        description: "test withdraw",
+      });
+    }).rejects.toBeInstanceOf(CreateStatementError.InsufficientFunds);
+  });
+});
